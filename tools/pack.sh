557ca07f5f6f0707f2a36722129de8d20b665d8d
Tian Liao <tilia@microsoft.com>
2024-12-03 14:32:28 +0800

    test

--- tools/pack.sh
+++ tools/pack.sh
@@ -3,4 +3,4 @@ file="CMakeLists.txt"
 version=$(grep -oP 'msft_proxy\s+VERSION\s+\K[0-9]+\.[0-9]+\.[0-9]+' "$file")
 git tag "$version"
 git push origin "$version"
-tar -czf "proxy.tgz" "proxy.h"
+tar -czf "proxy-$version.tgz" "proxy.h"
